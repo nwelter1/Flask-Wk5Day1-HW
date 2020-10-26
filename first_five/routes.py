fa086a2d58c7215b8f385efa7c28e0042ac26a85
nwelter1 <nwelter1@gmail.com>
2020-10-26 13:19:28 -0500

    typo update

--- first_five/routes.py
+++ first_five/routes.py
@@ -4,10 +4,10 @@ from flask import render_template
 #Athlete page route
 @app.route('/myfive')
 def myfive():
-    name = 'Top Five Athletes'
-    athlete_dict = {1:'Tony Hawk', 2:'Sage Kotsenberg', 3: 'Michael Jordan', 4: 'Payton Manning', 5:'Patrick Kane'}
-    return render_template('myfive.html',name=name, athlete_dict=athlete_dict)
+    athlete_dict = {1:'Tony Hawk', 2:'Sage Kotsenberg', 3: 'Michael Jordan', 4: 'Peyton Manning', 5:'Patrick Kane'}
+    return render_template('myfive.html', athlete_dict=athlete_dict)
 #home page route
 @app.route('/')
 def home():
-    return render_template('home.html')
+    name = 'Top Five Athletes'
+    return render_template('home.html',name=name)
